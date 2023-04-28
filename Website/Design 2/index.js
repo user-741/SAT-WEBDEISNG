61f750fdd209748dc0dd56fbc230119639aecc2f
user-741 <latifnaderi145@gmail.com>
2023-04-28 21:24:16 +1000

    New design

--- Website/Design 2/index.js
+++
@@ -1,6 +0,0 @@
-const resetButton = document.querySelector('.btn-secondary');
-
-resetButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  document.querySelector('form').reset();
-});
